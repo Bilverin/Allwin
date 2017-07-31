504ae7ddb5982e057d571c3a6c0887c3e0303608
Vasilchikov Aleksey <alekseyvi1303@gmail.com>
2017-07-31 18:13:01 +0300

    animation update last 31.07 18:12

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	
+
 	// to top button
 	if($(window).scrollTop() >= 750) {
 		$('.to-top').show();
@@ -30,7 +32,33 @@ $(document).ready(function() {
 	}); 
 
 	// partners carousel
-	$('.owl-carousel').owlCarousel();
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		autoplay: true,
+		center: true,
+		autoplayTimeout: 3000,
+		autoplaySpeed: 1000,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			991: {
+				items: 4
+			},
+			1199: {
+				items: 5
+			},
+			1440: {
+				items: 6
+			}
+		}
+	});
 
 	// scrollr js
 	var s = skrollr.init();
@@ -38,8 +66,51 @@ $(document).ready(function() {
 	// wow js
 	new WOW().init();
 
+	// testimonials animation
+	var testiLenght = $('.js-testiItems li').length;
+	$('.js-testiNavLeft').click(function() {
+		
+		$('.js-testiItems li.active').removeClass('active left').addClass('right').prev().removeClass('left right').addClass('active');
+
+		var activeIndex = $('.js-testiItems li.active').index();
+		if(activeIndex == 0) {
+			$(this).addClass('inactive');
+		}
+
+		$('.js-testiNavRight').removeClass('inactive');
+
+		return false;
+	})
+	$('.js-testiNavRight').click(function() {
+		
+		$('.js-testiItems li.active').removeClass('active right').addClass('left').next().removeClass('left right').addClass('active');
+
+		var activeIndex = $('.js-testiItems li.active').index();
+		if(activeIndex == (testiLenght - 1)) {
+			$(this).addClass('inactive');
+		}
+		$('.js-testiNavLeft').removeClass('inactive');
+
+		return false;
+	})
+
 });
 
 $(window).on('load', function() {
-	$('.intro-img').removeClass('inactive');
+	// scrolling to top
+	var windowOffset = $(window).scrollTop();
+	if(windowOffset != 0) {
+		setTimeout(function(){
+			window.scrollTo(0,0);
+		}, 10);
+	}
+
+	
+	setTimeout(function(){
+		// preloader
+		$('.preloader').fadeOut();
+
+		// intro img animation
+		$('.intro-img').removeClass('inactive');
+	}, 500);
 });
